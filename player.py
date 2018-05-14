95874072259326892c55572d1abda8b6a434e43b
mila <msruseva@gmail.com>
2018-05-14 21:22:57 +0300

    Refactor class Player
    Add helper methods: #player_name() and #reset().

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
-import pdb
-
-
 class Player:
 
-    def __init__(self):
-        pass
+    """Returns true or false
+
+    If the player has found the number of the other player returns true
+    otherwise returns false
+    """
 
     def bulls_and_cows(self, number, guess):
         bulls = 0
@@ -19,6 +19,18 @@ class Player:
                 else:
                     continue
 
-        print "Bulls {} and {} cows".format(bulls, cows)
+        print "{} found {} bulls and {} cows".format(self.player_name(), bulls, cows)
 
         return bulls == 4
+
+    """Return the name of the player
+    """
+
+    def player_name(self):
+        return "Unknown"
+
+    """Reset the number of the player
+    """
+
+    def reset(self):
+        self.number = None
